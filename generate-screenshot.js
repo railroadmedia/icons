6e80497bd56f64850aa7a0cd5a246275415eeeb1
Curtis <curtis@drumeo.com>
2019-11-19 13:52:24 -0800

    Remove generate screenshot script

--- generate-screenshot.js
+++
@@ -1,7 +0,0 @@
-const app = require('node-server-screenshot');
-app.fromHTML("<html><body>Hello world!</body></html>", '/app/icons/dist/icons.png', function(){
-    console.log('added an image');
-    //an image of google.com has been saved at ./test.png
-});
-
-module.exports = app;
